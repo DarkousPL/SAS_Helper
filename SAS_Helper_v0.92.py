3e4bdca4bb2e794a068bfe487fab715a4228fba2
Karol Janeczek <darkous@gmail.com>
2019-10-14 21:52:24 +0200

    ver 0.92 - with iteration of steps from the beginining

--- SAS_Helper_v0.92.py
+++ SAS_Helper_v0.92.py
@@ -9,13 +9,13 @@ import re as re
 # 4. Umożliwić iteracje od nowa kroków (Krok_001, Krok_002 itd) (ZROBIONE)
 
 # Parameters of program
-#selected_file = r'C:\Users\Darkous\01_SAS_Helper\Files\file1.txt'
-#output_file =   r'C:\Users\Darkous\01_SAS_Helper\Files\result1.txt'
+#selected_file = r'\\saswds\Risk\RISK_DATABASE\105_REFORECAST\105_04_Doposażenie_corr2.sas'
+#output_file =   r'\\saswds\Risk\RISK_DATABASE\105_REFORECAST\105_04_Doposażenie_corrrr.sas'
 
 ###############################################################################
 # Own parameters: which user can use or changes
 dict_param = {
-  "old_step": 'Step_',
+  "old_step": 'Krok_',
   "new_step": 'Krok_',
   "check_iter": 'n',
   "tab_start": '/*#',
@@ -36,6 +36,7 @@ def przetworz_plik(selected_file, output_file, **kwargs):
     start_var = False
     end_var = False
     ignore_text = False
+    replace_text = False
 
     with open(selected_file, 'r') as f:
         for line in f:
@@ -92,32 +93,41 @@ def przetworz_plik(selected_file, output_file, **kwargs):
                 string = ' '.join(sentences_dict[i])
                 if dict_param['old_step'] in string:
                     var = str(iter_krok).zfill(3)
-                    string = dict_param['new_step'] + var + string[re.search(dict_param['old_step'] + "\w+", string).end():]
+                    string = dict_param['new_step'] + var + string[re.search(dict_param['old_step'] + r"\w+", string).end():]
                     iter_krok += 1
                 w.write(string)
                 w.write('\n')
         w.write('\n <<<*/ \n\n')
 
     # rewrite old file to new file (exept old heading)
+        iter_krok = 1
         for old_line in file_list:
             if '/*>>>' in old_line:
                     ignore_text = True
             elif '<<<*/' in old_line:
                     ignore_text = False
 
-            if (ignore_text != True) & ('<<<*/' not in old_line):
-                    w.write(old_line)
+            if (not ignore_text) & ('<<<*/' not in old_line):
+#################################
+                if kwargs['check_iter'] == 'n': #standard rewrite
+                    w.write(old_line)               
+                elif kwargs['check_iter'] == 't':   #rewrite with replace
+                    if dict_param["tab_start"] in old_line:
+                        replace_text = True         #check that start replace
 
-# def show_parameters(tab_start=dict_param['tab_start'], 
-#         tab_end=dict_param['tab_end'], 
-#         old_step=dict_param['old_step'], 
-#         new_step=dict_param['new_step'], 
-#         check_iter=dict_param['check_iter']):
-#     print("Twoje aktualne parametry:\n- old_step = {} \n- new_step = {} \n- check_iter = {} \n- tab_start = {} \n- tab_end = {}".format(dict_param['old_step'],
-#                                                                                                          dict_param['new_step'],
-#                                                                                                          dict_param['check_iter'], 
-#                                                                                                          dict_param['tab_start'], 
-#                                                                                                          dict_param['tab_end']))
+                    if (dict_param['old_step'] in old_line) & replace_text:
+                        var = str(iter_krok).zfill(3)
+                        string = old_line[:old_line.rfind(dict_param['old_step'])] + dict_param['new_step'] + var + old_line[re.search(dict_param['old_step'] + r"\w+", old_line).end():]
+                        iter_krok += 1
+                        w.write(string)
+                        w.write('\n')
+                    else:
+                        w.write(old_line)    
+
+                    if (dict_param['tab_end'] in old_line) & replace_text:
+                        replace_text = False
+
+#################################
 
 # Function where user can change parameters
 def zmien_parametr(answer):
@@ -133,8 +143,8 @@ def zmien_parametr(answer):
         while odp not in ('t', 'n'):
             odp = input("Wprowadź wartość, może być 't' lub 'n': ")
             if odp in ('t', 'n'):
-                print("Zmieniono parametr na: ", dict_param['check_iter'])
                 dict_param['check_iter'] = odp
+                print("Zmieniono parametr na: ", dict_param['check_iter'])
             else:
                 print("Niepoprawny parametr. Wprowadź raz jeszcze.")
     else:
@@ -143,8 +153,9 @@ def zmien_parametr(answer):
 
 # ===============================================================================================================
 # MAIN PROGRAM 
-print("Witaj " + os.environ['userdomain'],
-      "\nWersja programu: 0.90",
+
+print("Witaj " + os.getlogin(),
+      "\nWersja programu: 0.92",
       "\n - Jeżeli chcesz zakończyć aplikację wpisz 'q'", 
       "\n - jak chcesz zmienić parametry wpisz 'config'",
       "\n - jak chcesz przetworzyć plik wpisz 'jedziemy' ",
@@ -152,6 +163,7 @@ print("Witaj " + os.environ['userdomain'],
 
 #MAIN LOOP
 while True:
+    print('------------------------------------\n')
     answer = input('Co takiego chcesz zrobić?:')
     if answer.lower().startswith("config"):
         zmien_parametr(input("Wpisz który parametr chcesz zmienić:"))
@@ -163,7 +175,7 @@ while True:
                                                                                                          dict_param['tab_start'], 
                                                                                                          dict_param['tab_end']))
     elif answer.lower().startswith("jedziemy"):
-        selected_file = input("Wprowadź ścieżkę i plik który chcesz przetworzyć: ")
+        selected_file = input("Wprowadź ścieżkę i plik który chcesz przetworzyć: ").replace("'",'').replace('"','')
         output_file = input("Wprowadź ścieżkę i plik który ma być plikiem wynikowym (kliknij enter aby użyć poprzedni wpis i wygenerować plik z dopiskiem _corr): ")
         if output_file == '':
             output_file = selected_file[:-4] + "_corr" + selected_file[-4:]
@@ -173,11 +185,9 @@ while True:
                                                     new_step=dict_param['new_step'], 
                                                     check_iter=dict_param['check_iter'])
         print("Plik został przetworzony. \nWynik przetworzenia: ", output_file)
+        print('Powrót do linii podstawowych komend.')
     elif answer.lower().startswith("q"):
         print("Program został zakończony.")
         sys.exit()
     else:
         print("Polecenia nie rozpoznano.")
-
-
-        
